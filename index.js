8ad7840f31b27cc7c0a92d5234f575da81dae4b1
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-01-23 19:49:10 +0100

    Query url for allowed ips

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 'use strict';
 
+const request = require('request');
+
 module.exports.title = 'IP Restriction';
 module.exports.init = function(app, done) {
 
+    let reject = (session) => {
+        let err = new Error('IP ' + session.remoteAddress +  ' not permitted to send!');
+        err.responseCode = 530;
+        return err;
+    }
+
     app.addHook('smtp:connect', (session, next) => {
+        let allowedIPs = app.config.allowedips;
 
         if (!app.config.interfaces.includes(session.interface)) {
             // not an interface we care about
             next();
         }
 
-        if (!app.config.allowedips.includes(session.remoteAddress)) {
-            let err = new Error('IP ' + session.remoteAddress +  ' not permitted to send!');
-            err.responseCode = 530;
-            return next(err);
-        }
+        // Config contains an url, checking against that
+        if (typeof allowedIPs === 'string' && (allowedIPs.includes("https") || allowedIPs.includes("http"))) {
+            let formData = {
+                ip: session.remoteAddress
+            };
+            request.post({url: allowedIPs, formData: formData}, (err, httpResponse, body) => {
+                if (err) {
+                    // error means reject, so reject connection
+                    return next(reject(session));
+                }
 
-        // IP seems to be legit
-        next();
+                // Response is a 200 so IP seems to be legit
+                return next();
+            });
+        } else if (Array.isArray(allowedIPs)) {
+            if (!allowedIPs.includes(session.remoteAddress)) {
+                return next(reject(session));
+            }
+            // IP seems to be legit
+            return next();
+        } else {
+            // unexpected config, deny connection for security reasons
+            return next(reject(session));
+        }
     });
 
     done();
