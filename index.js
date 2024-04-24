319ebfc1af80173634f0b9c7b52594e1c425dd23
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-04-24 14:36:15 +0200

    Use net.BlockList instead of array lookup

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 'use strict';
 
+const net = require('net');
+
+const _allowlist = new net.BlockList();
+
 module.exports.title = 'IP Restriction';
 module.exports.init = function(app, done) {
 
-    app.addHook('smtp:connect', (session, next) => {
+    for (let ip of app.config.allowedips) {
+        if (ip.includes('/')) {
+            const [ip, mask] = ip.split('/');
+            _allowlist.addSubnet(ip, mask);
+        } else {
+            _allowlist.addAddress(ip);
+        }
+    }
 
+    app.addHook('smtp:connect', (session, next) => {
         if (!app.config.interfaces.includes(session.interface)) {
             // not an interface we care about
             next();
         }
 
-        if (!app.config.allowedips.includes(session.remoteAddress)) {
+        if (!_allowlist.check(session.remoteAddress)) {
             let err = new Error('IP ' + session.remoteAddress +  ' not permitted to send!');
             err.responseCode = 530;
             return next(err);
